90230a9e387dbddd27da6292fa0f6745d4317ba5
Brenden Perry <brendenperry5@gmail.com>
2025-08-19 18:03:24 -0700

    Add KeyValuePair extension to convert to tuple
    Introduced KeyValuePairExtension with a ToTuple method for converting KeyValuePair instances to tuples. Updated project version to 1.0.0.6.

---
+++ DeepSigma.General/Extensions/KeyValuePairExtension.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DeepSigma.General.Extensions
+{
+    /// <summary>
+    /// Extension methods for KeyValuePairs.
+    /// </summary>
+    public static class KeyValuePairExtension
+    {
+        /// <summary>
+        /// Converts a KeyValuePair to a tuple.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="kvp"></param>
+        /// <returns></returns>
+        public static (TKey Key, TValue Value) ToTuple<TKey, TValue>(this KeyValuePair<TKey, TValue> kvp)
+        {
+            return (kvp.Key, kvp.Value);
+        }
+    }
+}
