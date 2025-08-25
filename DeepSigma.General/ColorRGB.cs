fd16c29c744350e02db8690178497f562b718e30
Brenden Perry <brendenperry5@gmail.com>
2025-08-25 15:15:15 -0700

    Update ColorRGB.cs

--- DeepSigma.General/ColorRGB.cs
+++ DeepSigma.General/ColorRGB.cs
@@ -57,5 +57,44 @@ namespace DeepSigma.General
         {
             return new ColorRGB(values.red, values.green, values.blue);
         }
+
+        /// <summary>
+        /// Interpolates between two ColorRGB instances based on a given fraction.
+        /// </summary>
+        /// <param name="color1">
+        /// The starting color (fraction = 0).
+        /// </param>
+        /// <param name="color2">
+        /// The ending color (fraction = 1).
+        /// </param>
+        /// <param name="fraction">
+        /// A value between 0 and 1 where 0 corresponds to color1 and 1 corresponds to color2.
+        /// </param>
+        /// <returns></returns>
+        public static ColorRGB Interpolate(ColorRGB color1, ColorRGB color2, float fraction)
+        {
+            byte red = (byte)(color1.Red + (color2.Red - color1.Red) * fraction);
+            byte green = (byte)(color1.Green + (color2.Green - color1.Green) * fraction);
+            byte blue = (byte)(color1.Blue + (color2.Blue - color1.Blue) * fraction);
+            return new ColorRGB(red, green, blue);
+        }
+
+        /// <summary>
+        /// Calculates the Euclidean distance between two ColorRGB instances.
+        /// </summary>
+        /// <param name="color1">
+        /// The first color.
+        /// </param>
+        /// <param name="color2">
+        /// The second color.
+        /// </param>
+        /// <returns></returns>
+        public static double Difference(ColorRGB color1, ColorRGB color2)
+        {
+            int redDiff = color1.Red - color2.Red;
+            int greenDiff = color1.Green - color2.Green;
+            int blueDiff = color1.Blue - color2.Blue;
+            return Math.Sqrt(redDiff * redDiff + greenDiff * greenDiff + blueDiff * blueDiff);
+        }
     }
 }
