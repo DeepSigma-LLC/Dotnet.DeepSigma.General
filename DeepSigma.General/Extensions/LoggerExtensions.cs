141a2ea40dda31bb1e14c3527a3f9b5d7dd09a8c
Brenden Perry <brendenperry5@gmail.com>
2025-12-03 14:43:44 -0800

    Enhance LoggerExtensions with flexible logging methods
    Refactored LoggerExtensions to support variadic arguments and improved overloads for error, warning, and information logging. Added new methods for conditional logging based on exception presence and updated XML documentation with usage examples. Incremented project version to 1.7.1.0.

--- DeepSigma.General/Extensions/LoggerExtensions.cs
+++ DeepSigma.General/Extensions/LoggerExtensions.cs
@@ -12,49 +12,131 @@ public static class LoggerExtensions
     /// Tries to log an error message with an optional exception.
     /// Note: If the logger is null (or the error is null), no action is taken.
     /// </summary>
+    /// <remarks>
+    /// <code>
+    /// // Usage example:
+    /// log.TryToLogErrorOnlyIfException(ex, "An error occurred while processing the request. {RequestId}", requestId);
+    /// </code>
+    /// </remarks>
     /// <param name="Logger"></param>
     /// <param name="exception"></param>
     /// <param name="message"></param>
-    public static void TryToLogErrorOnlyIfException(this ILogger? Logger, Exception? exception, string? message)
+    /// <param name="args"></param>
+    public static void TryToLogErrorOnlyIfException(this ILogger? Logger, Exception? exception, string? message, params object?[] args)
     {
         if (exception == null) return;
-        Logger.TryToLogError(message, exception);
+        Logger.TryToLogError(exception, message, args);
+    }
+
+    /// <summary>
+    /// Tries to log a warning message with an optional exception.
+    /// Note: If the logger is null (or the error is null), no action is taken.
+    /// </summary>
+    /// <remarks>
+    /// <code>
+    /// // Usage example:
+    /// log.TryToLogWarningOnlyIfException(ex, "An error occurred while processing the request. {RequestId}", requestId);
+    /// </code>
+    /// </remarks>
+    /// <param name="Logger"></param>
+    /// <param name="exception"></param>
+    /// <param name="message"></param>
+    /// <param name="args"></param>
+    public static void TryToLogWarningOnlyIfException(this ILogger? Logger, Exception? exception, string? message, params object?[] args)
+    {
+        if (exception == null) return;
+        Logger.TryToLogWarning(exception, message, args);
+    }
+
+    /// <summary>
+    /// Tries to log an information message with an optional exception.
+    /// Note: If the logger is null (or the error is null), no action is taken.
+    /// </summary>
+    /// <remarks>
+    /// <code>
+    /// // Usage example:
+    /// log.TryToLogInformationOnlyIfException(ex, "Currently on request: {RequestId}", requestId);
+    /// </code>
+    /// </remarks>
+    /// <param name="Logger"></param>
+    /// <param name="exception"></param>
+    /// <param name="message"></param>
+    /// <param name="args"></param>
+    public static void TryToLogInformationOnlyIfException(this ILogger? Logger, Exception? exception, string? message, params object?[] args)
+    {
+        if (exception == null) return;
+        Logger.TryToLogInformation(exception, message, args);
     }
 
     /// <summary>
     /// Tries to log an error message with an optional exception.
     /// Note: If the logger is null, no action is taken.
     /// </summary>
+    /// <remarks>
+    /// <code>
+    /// // Usage example:
+    /// log.TryToLogError(ex, "An error occurred while processing the request. {RequestId}", requestId);
+    /// </code>
+    /// </remarks>
     /// <param name="Logger"></param>
     /// <param name="message"></param>
     /// <param name="exception"></param>
-    public static void TryToLogError(this ILogger? Logger, string? message, Exception? exception)
+    /// <param name="args"></param>
+    public static void TryToLogError(this ILogger? Logger, Exception? exception, string? message, params object?[] args)
     {
-        Logger?.LogError(exception, message);
+        Logger?.LogError(exception, message, args);
     }
-
 
     /// <summary>
     /// Tries to log an information message with an optional exception.
     /// Note: If the logger is null, no action is taken.
     /// </summary>
+    /// <remarks>
+    /// <code>
+    /// // Usage example:
+    /// log.TryToLogInformation(ex, "Invalid request made. {RequestId}", requestId);
+    /// </code>
+    /// </remarks>
     /// <param name="Logger"></param>
     /// <param name="message"></param>
     /// <param name="exception"></param>
-    public static void TryToLogInformation(this ILogger? Logger, string? message, Exception? exception)
+    /// <param name="args"></param>
+    public static void TryToLogInformation(this ILogger? Logger, Exception? exception, string? message, params object?[] args)
     {
-        Logger?.LogInformation(message);
+        if (Logger is null) return;
+        if (Logger.IsEnabled(LogLevel.Information) == true) return;
+        if (exception is not null)
+        {
+            Logger.LogInformation(exception, message ?? string.Empty, args);
+            return;
+        }
+        Logger.LogInformation(message ?? string.Empty, args);
     }
 
     /// <summary>
     /// Tries to log a warning message with an optional exception.
     /// Note: If the logger is null, no action is taken.
     /// </summary>
+    /// <remarks>
+    /// <code>
+    /// // Usage example:
+    /// log.TryToLogWarning(ex, "Request properties not set. {RequestId}", requestId);
+    /// </code>
+    /// </remarks>
     /// <param name="Logger"></param>
     /// <param name="message"></param>
     /// <param name="exception"></param>
-    public static void TryToLogWarning(this ILogger? Logger, string message, Exception? exception)
+    /// <param name="args"></param>
+    public static void TryToLogWarning(this ILogger? Logger, Exception? exception, string? message, params object?[] args)
     {
-        Logger?.LogWarning(exception, message);
+        if (Logger is null) return;
+        if (Logger.IsEnabled(LogLevel.Warning) != true) return;
+
+        if (exception is not null)
+        {
+            Logger.LogWarning(exception, message ?? string.Empty, args);
+            return;
+        }
+        Logger.LogWarning(message ?? string.Empty, args);
     }
 }
