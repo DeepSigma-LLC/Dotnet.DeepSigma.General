2166c32a28c2e86bb754388d86202ef667176a15
Brenden Perry <brendenperry5@gmail.com>
2025-12-03 07:55:25 -0800

    Add LoggerExtensions with safe logging methods
    Introduces LoggerExtensions providing safe extension methods for ILogger to log errors, information, and warnings, handling null loggers and exceptions gracefully. Also bumps project version to 1.7.0.0.

---
+++ DeepSigma.General/Extensions/LoggerExtensions.cs
@@ -0,0 +1,60 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace DeepSigma.General.Extensions;
+
+/// <summary>
+/// Extension methods for ILogger.
+/// </summary>
+public static class LoggerExtensions
+{
+    /// <summary>
+    /// Tries to log an error message with an optional exception.
+    /// Note: If the logger is null (or the error is null), no action is taken.
+    /// </summary>
+    /// <param name="Logger"></param>
+    /// <param name="exception"></param>
+    /// <param name="message"></param>
+    public static void TryToLogErrorOnlyIfException(this ILogger? Logger, Exception? exception, string? message)
+    {
+        if (exception == null) return;
+        Logger.TryToLogError(message, exception);
+    }
+
+    /// <summary>
+    /// Tries to log an error message with an optional exception.
+    /// Note: If the logger is null, no action is taken.
+    /// </summary>
+    /// <param name="Logger"></param>
+    /// <param name="message"></param>
+    /// <param name="exception"></param>
+    public static void TryToLogError(this ILogger? Logger, string? message, Exception? exception)
+    {
+        Logger?.LogError(exception, message);
+    }
+
+
+    /// <summary>
+    /// Tries to log an information message with an optional exception.
+    /// Note: If the logger is null, no action is taken.
+    /// </summary>
+    /// <param name="Logger"></param>
+    /// <param name="message"></param>
+    /// <param name="exception"></param>
+    public static void TryToLogInformation(this ILogger? Logger, string? message, Exception? exception)
+    {
+        Logger?.LogInformation(message);
+    }
+
+    /// <summary>
+    /// Tries to log a warning message with an optional exception.
+    /// Note: If the logger is null, no action is taken.
+    /// </summary>
+    /// <param name="Logger"></param>
+    /// <param name="message"></param>
+    /// <param name="exception"></param>
+    public static void TryToLogWarning(this ILogger? Logger, string message, Exception? exception)
+    {
+        Logger?.LogWarning(exception, message);
+    }
+}
