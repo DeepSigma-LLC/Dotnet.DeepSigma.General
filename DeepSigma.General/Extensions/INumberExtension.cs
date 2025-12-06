43fecbd24642a1b047d964c4bf0935ac27d140d0
Brenden Perry <brendenperry5@gmail.com>
2025-12-05 22:28:43 -0800

    Add INumberExtension with IsCloserToZeroThan method
    Introduces a new static class INumberExtension providing an IsCloserToZeroThan extension method for INumber types. Also updates the project version to 1.8.2.0.

---
+++ DeepSigma.General/Extensions/INumberExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace DeepSigma.General.Extensions;
+
+/// <summary>
+/// Provides extension methods for INumber types.
+/// </summary>
+public static class INumberExtension
+{
+    /// <summary>
+    /// Determines whether the specified value is closer to zero than the compareTo value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value"></param>
+    /// <param name="compareTo"></param>
+    /// <returns></returns>
+    public static bool IsCloserToZeroThan<T>(this T value, T compareTo)
+        where T : INumber<T>
+    {
+        return T.Abs(value) < T.Abs(compareTo);
+    }
+}
