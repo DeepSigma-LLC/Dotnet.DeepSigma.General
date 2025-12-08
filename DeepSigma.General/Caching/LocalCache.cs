72a197c31fd55d91296f8f1fea5057f4ef1d884f
Brenden Perry <brendenperry5@gmail.com>
2025-12-07 19:46:46 -0800

    Enhance LocalCache with refresh and enumeration support
    Added support for cache refresh via external methods, batch retrieval, and enumeration by implementing IEnumerable. Refactored add methods for clarity, improved expired item handling, and exposed additional utility methods such as Contains and Count. Updated project version to 1.8.5.0.

--- DeepSigma.General/Caching/LocalCache.cs
+++ DeepSigma.General/Caching/LocalCache.cs
@@ -1,4 +1,5 @@
 ﻿using DeepSigma.General.Extensions;
+using System.Collections;
 
 namespace DeepSigma.General.Caching;
 
@@ -7,7 +8,7 @@ namespace DeepSigma.General.Caching;
 /// </summary>
 /// <typeparam name="TIndex"></typeparam>
 /// <typeparam name="TValue"></typeparam>
-public class LocalCache<TIndex, TValue> 
+public class LocalCache<TIndex, TValue> : IEnumerable<KeyValuePair<TIndex, CacheItem<TValue>>>
     where TIndex : notnull
     where TValue : class
 {
@@ -21,10 +22,51 @@ public class LocalCache<TIndex, TValue>
     /// </summary>
     private TimeSpan TimeToLive { get; init; }
 
+    /// <summary>
+    /// The method to get the value from external source.
+    /// </summary>
+    private Func<TIndex, TValue>? GetValueMethod { get; set; }
+
+    /// <summary>
+    /// The method to get multiple values from external source.
+    /// </summary>
+    private Func<List<TIndex>, List<KeyValuePair<TIndex, TValue>>>? GetValuesMethod { get; set; }
+
     /// <inheritdoc cref="LocalCache{TIndex, TValue}"/>
-    public LocalCache(TimeSpan time_to_live)
+    public LocalCache(TimeSpan time_to_live, Func<TIndex, TValue>? get_value_method = null, Func<List<TIndex>, List<KeyValuePair<TIndex, TValue>>>? get_values_method = null)
     {
         TimeToLive = time_to_live;
+        GetValueMethod = get_value_method;
+        GetValuesMethod = get_values_method;
+    }
+
+    /// <summary>
+    /// Sets the method to retrieve the value externally. 
+    /// </summary>
+    /// <param name="get_value_method"></param>
+    public void SetGetValueMethod(Func<TIndex, TValue> get_value_method)
+    {
+        GetValueMethod = get_value_method;
+    }
+
+    /// <summary>
+    /// Sets the method to retrieve multiple values externally.
+    /// </summary>
+    /// <param name="get_values_method"></param>
+    public void SetGetValuesMethod(Func<List<TIndex>, List<KeyValuePair<TIndex, TValue>>> get_values_method)
+    {
+        GetValuesMethod = get_values_method;
+    }
+
+    /// <summary>
+    /// Adds a new item to the cache.
+    /// If the item already exists, it should be updated.
+    /// </summary>
+    /// <param name="item_index"></param>
+    /// <param name="item_value"></param>
+    public void Add(TIndex item_index, TValue item_value)
+    {
+        cache[item_index] = new CacheItem<TValue>(item_value, TimeToLive);
     }
 
     /// <summary>
@@ -33,57 +75,108 @@ public class LocalCache<TIndex, TValue>
     /// </summary>
     /// <param name="item_index"></param>
     /// <param name="item_value"></param>
-    public void AddWithNoUpdate(TIndex item_index, TValue item_value)
+    public void AddOnlyNotUpdate(TIndex item_index, TValue item_value)
     {
-        if (cache.ContainsKey(item_index))
-        {
-            if(cache[item_index].IsExpired == false) return;
-            cache.Remove(item_index);
-        }
+        (bool Removed, var Value) = RemoveIfExpired(item_index);
+        if (Removed == false && Value is not null) return;
         cache[item_index] = new(item_value, TimeToLive);
     }
 
     /// <summary>
-    /// Adds a new item to the cache.
-    /// </summary>
-    /// <param name="item_index"></param>
-    /// <param name="item_value"></param>
-    public void AddWithUpdate(TIndex item_index, TValue item_value)
-    {
-        cache.TryAdd(item_index, new CacheItem<TValue>(item_value, TimeToLive));
-    }
-
-    /// <summary>
     /// Tries to get a value from the cache.
+    /// If the item is expired, it is removed and null is returned.
     /// </summary>
     /// <param name="index"></param>
     /// <returns></returns>
     public TValue? TryGet(TIndex index)
     {
-        cache.TryGetValue(index, out CacheItem<TValue>? value);
-        if(value is null) return null;
-        if(value.IsExpired)
-        {
-            cache.Remove(index);
-            return null;
-        }
-        return value.Value;
+        (bool Removed, var Value) = RemoveIfExpired(index);
+        if (Removed) return null;
+        return Value?.Value;
     }
 
     /// <summary>
-    /// Cleans up expired items from the cache.
+    /// Tries to get multiple values from the cache.
+    /// If an item is expired, it is removed and not returned.
+    /// </summary>
+    /// <param name="items"></param>
+    /// <returns></returns>
+    public IEnumerable<KeyValuePair<TIndex, TValue>> TryGetMultiple(List<TIndex> items)
+    {
+        foreach (TIndex index in items)
+        {
+            TValue? cached_value = TryGet(index);
+            if (cached_value is not null)
+            {
+                yield return new KeyValuePair<TIndex, TValue>(index, cached_value);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Tries to get a value from the cache.
+    /// It will remove expired items and attempt to refresh the item using GetValueMethod.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public TValue? TryGetWithCacheRefresh(TIndex index)
+    {
+        TValue? result_from_cache = TryGet(index);
+        if(result_from_cache is not null) return result_from_cache;
+        if (GetValueMethod is null) return null;
+
+        TValue? value = GetValueMethod(index);
+        if (value is null) return null;
+        cache.GetOrAdd(index, new CacheItem<TValue>(value, TimeToLive));
+        return value;
+    }
+
+    /// <summary>
+    /// Tries to get multiple values from the cache. 
+    /// It will remove expired items and attempt to refresh missing items using GetValuesMethod.
+    /// </summary>
+    /// <param name="indices"></param>
+    /// <returns></returns>
+    public IEnumerable<KeyValuePair<TIndex, TValue>> TryGetMultipleWithCacheRefresh(List<TIndex> indices)
+    {
+        List<TIndex> indices_to_fetch = [];
+        foreach (TIndex index in indices)
+        {
+            TValue? cached_value = TryGet(index);
+            if (cached_value is not null)
+            {
+                yield return new KeyValuePair<TIndex, TValue>(index, cached_value);
+            }
+            else
+            {
+                indices_to_fetch.Add(index);
+            }
+        }
+
+        if (indices_to_fetch.Count > 0 && GetValuesMethod is not null)
+        {
+            List<KeyValuePair<TIndex, TValue>> fetched_values = GetValuesMethod(indices_to_fetch);
+            foreach (var kvp in fetched_values)
+            {
+                cache[kvp.Key] = new CacheItem<TValue>(kvp.Value, TimeToLive);
+                yield return kvp;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Cleans up all expired items from the cache.
     /// </summary>
     public void RemoveAllExpiredItems()
     {
-        List<TIndex> keys_to_remove = cache
+        IEnumerable<TIndex> keys_to_remove = cache
             .Where(kvp => kvp.Value.IsExpired)
-            .Select(kvp => kvp.Key)
-            .ToList();
+            .Select(kvp => kvp.Key);
         keys_to_remove.ForEach(key => cache.Remove(key));
     }
 
     /// <summary>
-    /// Removes a specific item from the cache.
+    /// Removes a specific item from the cache by its index.
     /// </summary>
     /// <param name="index"></param>
     public void Remove(TIndex index)
@@ -92,10 +185,56 @@ public class LocalCache<TIndex, TValue>
     }
 
     /// <summary>
-    /// Clears all exchange rates from the cache.
+    /// Clears all values from the cache.
     /// </summary>
     public void Clear()
     {
         cache.Clear();
+    }
+
+    /// <summary>
+    /// Checks if the cache contains a specific item by its index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public bool Contains(TIndex index)
+    {
+        (bool Removed, var Value) = RemoveIfExpired(index);
+        return Removed == false && Value is not null;
+    }
+
+    /// <summary>
+    /// Gets the number of items in the cache.
+    /// </summary>
+    public int Count => cache.Count;
+
+    /// <inheritdoc/>
+    public IEnumerator<KeyValuePair<TIndex, CacheItem<TValue>>> GetEnumerator()
+    {
+         return cache.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    /// <summary>
+    /// Removes the item if it is expired.
+    /// Note: The item is not removed if it does not exist.
+    /// Also, the reference is returned for further processing if needed to improve performance by avoiding double lookups.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    private (bool Removed, CacheItem<TValue>? Value) RemoveIfExpired(TIndex index)
+    {
+        bool found = cache.TryGetValue(index, out CacheItem<TValue>? value);
+        if (value is null) return (false, null);
+        if (found && value.IsExpired)
+        {
+            cache.Remove(index);
+            return (true, null);
+        }
+        return (false, value);
     }
 }
