f855e7ec602a4660c6b3d491100677e97f50c17b
Brenden Perry <brendenperry5@gmail.com>
2025-12-08 15:36:08 -0800

    Add ComparableReferenceType for value-based equality
    Introduced an abstract ComparableReferenceType class that provides value-based equality comparison using a required UniqueId property. Updated project version to 1.8.6.0.

---
+++ DeepSigma.General/ComparableReferenceType.cs
@@ -0,0 +1,23 @@
+﻿
+namespace DeepSigma.General;
+
+/// <summary>
+/// A reference type that implements value-based equality comparison.
+/// </summary>
+public abstract class ComparableReferenceType
+{
+    /// <summary>
+    /// A unique identifier for this instance.
+    /// </summary>
+    public required string UniqueId { get; init; }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+    {
+        if (obj is not ComparableReferenceType other) return false;
+        return UniqueId == other.UniqueId;
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => UniqueId.GetHashCode();
+}
