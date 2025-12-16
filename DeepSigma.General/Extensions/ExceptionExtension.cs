177f58b343e0844df40bbe9b8b4b1f05bbbe8270
Brenden Perry <brendenperry5@gmail.com>
2025-12-15 19:16:34 -0800

    Add Exception extension method ThrowIfNotNull
    Introduces an extension method for the Exception class that throws the exception if it is not null. This utility simplifies conditional exception throwing.

---
+++ DeepSigma.General/Extensions/ExceptionExtension.cs
@@ -0,0 +1,17 @@
+﻿
+namespace DeepSigma.General.Extensions;
+
+/// <summary>
+/// Extension methods for the Exception class.
+/// </summary>
+public static class ExceptionExtension
+{
+    /// <summary>
+    /// Throws the exception if it is not null.
+    /// </summary>
+    /// <param name="exception"></param>
+    public static void ThrowIfNotNull(this Exception? exception)
+    {
+        if (exception is not null) throw exception;
+    }
+}
